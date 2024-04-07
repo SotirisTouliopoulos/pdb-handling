2dabdf89e06effb2c8e6374ee107fdb7c5da4e23
SotirisTouliopoulos <s.v.touliopoulos@gmail.com>
2024-04-07 17:09:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,26 @@ from src.input import Input
 import sys
 import shutil
 
-if '-d' in sys.argv:
+
+def major_flag(argv_list):
+    major_flag = []
+    for argv in sys.argv:
+        if len(argv) == 2 and argv[0] == '-':
+            major_flag.append(argv)
+
+    if len(major_flag) == 2:
+        major_flag.remove('-i')
+        major_flag.remove('-o')
+        flag = major_flag[0]
+        return flag
+    
+    elif len(major_flag) > 2:
+        print('Too many flags')
+    else:
+        print('Provide a flag')
+
+
+def download_function(argv_list):
     
     i_index = sys.argv.index('-i')
     
@@ -24,6 +43,18 @@ if '-d' in sys.argv:
     structure.wget()
     
     shutil.rmtree("src/__pycache__")
-                
-elif '-p' in sys.argv:
+
+
+def parse_function():
     pass
+
+
+def call_function(flag):
+    if flag == "-d":
+        download_function(sys.argv)
+    elif flag == "-p":
+        pass
+    
+    
+flag = major_flag(sys.argv)
+call_function(flag)
